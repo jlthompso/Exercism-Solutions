e7bb7eb60b6948a337af7e014b4adc5e14df8038
Joe Thompson <jthompson7@blueorigin.com>
2022-03-04 15:32:55 -0800

    Added solution.

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -1,23 +1,30 @@
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+EXPECTED_BAKE_TIME = 40  # minutes
+PREPARATION_TIME = 2  # minutes per layer
 
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
-    """Calculate the bake time remaining.
-
-    :param elapsed_bake_time: int baking time already elapsed.
-    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
-
-    Function that takes the actual minutes the lasagna has been in the oven as
-    an argument and returns how many minutes the lasagna still needs to bake
-    based on the `EXPECTED_BAKE_TIME`.
+def bake_time_remaining(elapsed_bake_time):
     """
+    Calculates the remaining bake time of the lasagna.
+    :param elapsed_bake_time: minutes the lasagna has baked.
+    :return: minutes remaining to bake the lasagna.
+    """
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
-    pass
 
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
+def preparation_time_in_minutes(layers):
+    """
+    Calculates the time required to prep the lasagna before baking.
+    :param layers: number of layers in the lasagna.
+    :return: minutes of prep time.
+    """
+    return PREPARATION_TIME * layers
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+
+def elapsed_time_in_minutes(layers, elapsed_bake_time):
+    """
+    Calculates the total time spent cooking the lasagna including bake and prep.
+    :param layers: number of layers in the lasagna.
+    :param elapsed_bake_time: minutes the lasagna has baked.
+    :return: total minutes of prep and elapsed bake time.
+    """
+    return preparation_time_in_minutes(layers) + elapsed_bake_time
